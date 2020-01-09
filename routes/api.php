09a2adb829c73ab32b2ffbd76eb19686584df23e
rom <romcrazy13@gmail.com>
2020-01-09 13:14:54 +0200

    start

--- routes/api.php
+++ routes/api.php
@@ -2,16 +2,15 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/', function () {
+        return true;
+    });
+    Route::post('/', function () {
+        return true;
+    });
+});
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
